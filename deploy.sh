db194074fbb3ca92a56bff9505b25d49061f5bc3
Eamonn O'Brien-Strain <eob@well.com>
2013-03-02 15:15:21 -0800

    exclude .git files

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 set -x
-s3cmd '--add-header=Cache-Control:public, max-age=300' --acl-public --exclude .git --exclude deploy.sh --exclude \*~ sync ./ s3://eamonn.obrien-strain.com
+s3cmd '--add-header=Cache-Control:public, max-age=300' --acl-public --exclude=.git\* --exclude=deploy.sh --exclude=\*~ sync ./ s3://eamonn.obrien-strain.com
